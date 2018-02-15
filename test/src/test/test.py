0bbd342b545aa39e54cf4e5e29ba4923a8cb9a4d
JCHAV106 <JCHAV106@WGC100G8XN662.santafe.ford.com>
2018-02-15 16:04:03 -0600

    Working on the xml2exl function

--- test/src/test/test.py
+++ test/src/test/test.py
@@ -5,111 +5,154 @@ Created on 08/02/2018
          Python Software Engineer
          FORD CSAP
 '''
-
-from os.path import join, dirname, abspath
 import xlrd
+from openpyxl import load_workbook
 import xml.etree.ElementTree as ET
+from openpyxl.reader.excel import KEEP_VBA
 
-#List variables to put the names of the bushing names contained in the xml and excel file
-xml_bushings = []
-exl_bushings = []
 
-#Assign excel workbook path to fname variable
-fname = 'C:\Users\JCHAV106\git\Scripts\test\src\test_data\Extract_bushing_rates_tool.xlsm'
-#Assign Inline .xml file to xname vairable
-xname = join(dirname(dirname(abspath(__file__))),'test_data', 'INLINEC.xml')
-#Assign file that contains the changes realised to the inline.xml file 
-oname = join(dirname(dirname(abspath(__file__))),'test_data', 'output2.xml')
 
-#Open the workbook
-x1_workbook = xlrd.open_workbook(fname)
+def exl2xml():
+    #Open the workbook to read in
+    x1_workbook = xlrd.open_workbook('C:\\Users\\JCHAV106\\git\\Scripts\\test\\src\\test_data\\Extract_bushing_rates_tool.xlsm')
+    
+    #Obtain spreadsheet names of the current workbook
+    sheet_names = x1_workbook.sheet_names()
+    print('Sheet Names',sheet_names)
+    
+    #Call the first sheet of the book
+    x1_sheet = x1_workbook.sheet_by_index(0)
+    
+    #List variables to put the names of the bushing names contained in the xml and excel file
+    xml_bushings = []
+    exl_bushings = []
+    #Iterate over inline.xml file to obtain all the bushing names 
+    for child in root.iter("NVHC_PROPERTY"):
+        for child2 in child.iter("NAME"):
+            xml_bushings.insert(0, child2.text)
+    print(xml_bushings)
 
-#Obtain spreadsheet names of the current workbook
-sheet_names = x1_workbook.sheet_names()
-print('Sheet Names',sheet_names)
+    cont = 0
+    bushings_dict = {}
 
-tree = ET.parse(xname)
+    for row_idx in range(0, x1_sheet.nrows):    # Iterate through rows
+        for col_idx in range(0, x1_sheet.ncols):  # Iterate through columns
+            cell_obj = x1_sheet.cell(row_idx, col_idx).value  # Get cell object by row, col 
+            if cell_obj == "Bushing":
+                for col_idx in range(col_idx, x1_sheet.ncols):    # Iterate through rows
+                    for row_idx in range(row_idx, x1_sheet.nrows): # Iterate through columns
+                        cell_obj2 = x1_sheet.cell(row_idx, col_idx).value
+                        for i in range(xml_bushings.__len__()):
+                            if cell_obj2 == xml_bushings[i]:
+                                r = row_idx 
+                                #print(cell_obj2, xml_bushings[i], i )
+                                for col_idx2 in range(col_idx+1, 10):    # Iterate through rows
+                                    for row_idx in range(col_idx+1, 6):  # Iterate through columns
+                                        cell_obj3 = x1_sheet.cell(r, col_idx2).value
+                                        #print(cell_obj3,cell_obj2, xml_bushings[i], a + 1)
+                                        exl_bushings.insert(0, cell_obj3)
+                                        cont = cont + 1
+                                        if cont == 6:
+                                            cont = 0
+                                            bushings_dict[str(cell_obj2)] = exl_bushings
+                                            exl_bushings = [] 
+                                            break
+                                    
+                                        break
+
+    print(bushings_dict)
+
+    #Iterate trough all the NVHC PROPERTY tags contained in the file
+    for child3 in root.iter("NVHC_PROPERTY"):
+        #Iterate trough all the NAME tags that are child of each NVHC PROPERTY tag
+        for child4 in child3.iter("NAME"):
+            #Iterate trough items of the bushing_dict to obtain their keys and values
+            for key in bushings_dict.items():
+                #Iterate to compare if the NAME obtained from the Bushing matches with one of the keys of the bushings_dict
+                if child4.text == key:
+                    print(key)
+                    #Accesign to each element of the list that is inside the found key
+                    rt1 = bushings_dict[child4.text][0]
+                    rt2 = bushings_dict[child4.text][1] 
+                    rt3 = bushings_dict[child4.text][2] 
+                    rt4 = bushings_dict[child4.text][3] 
+                    rt5 = bushings_dict[child4.text][4] 
+                    rt6 = bushings_dict[child4.text][5]
+                    #Iterate trough all the PARAMETERS tags that are child of each NVHC PROPERTY tag  
+                    for child4 in child3.iter("PARAMETERS"):
+                        #Iterates trough the content of the PARAMMETERS tag to find the busjing values(stiffnes(k) and ge) and replace each 
+                        #one of them with the corresponding value obtained from the excel file
+                        for child5 in child4:
+                            #Compare if the child tag of PARAMETERS matches with one of the values that gonna be replaced
+                            if child5.tag == "B_VALUES":
+                                child5.set("b1", str(0))
+                                child5.set("b2", str(0))
+                                child5.set("b3", str(0))
+                                child5.set("b4", str(0))
+                                child5.set("b5", str(0))
+                                child5.set("b6", str(0))
+                                #Write the new data to the same xml file or create a new with diferent name 
+                                tree.write('C:\\Users\\JCHAV106\\git\\Scripts\\test\\src\\test_data\\output2.xml')  
+                                print(child5.attrib)
+                            
+                            elif child5.tag == "K_VALUES":
+                                child5.set("k1", str(rt6))
+                                child5.set("k2", str(rt5))
+                                child5.set("k3", str(rt4))
+                                child5.set("k4", str(rt3))
+                                child5.set("k5", str(rt2))
+                                child5.set("k6", str(rt1))
+                                #Write the new data to the same xml file or create a new with diferent name  
+                                tree.write('C:\\Users\\JCHAV106\\git\\Scripts\\test\\src\\test_data\\output2.xml')  
+                                print(child5.attrib)
+
+def xml2exl():
+    #Open the workbook to write in
+    x1_workbook = xlrd.open_workbook('C:\\Users\\JCHAV106\\git\\Scripts\\test\\src\\test_data\\Extract_bushing_rates_tool - Copy.xlsm')
+    #Open the workbook to read in
+    y1_workbook = load_workbook('C:\\Users\\JCHAV106\\git\\Scripts\\test\\src\\test_data\\Extract_bushing_rates_tool - Copy.xlsm')
+    wb = y1_workbook.active
+    
+    x1_sheet = x1_workbook.sheet_by_index(1)
+    
+    #List variables to put the names of the bushing names contained in the xml and excel file
+    xml_bushings = {}
+    exl_bushings = []
+    
+    #Iterate over inline.xml file to obtain all the bushing names 
+    for child in root.iter("NVHC_PROPERTY"):
+        for child2 in child.iter("NAME"):
+            print(child2.text)
+            for child3 in child.iter("PARAMETERS"):
+                print(child3.tag)
+                for child4 in child3:
+                    if child4.tag == "GE_VALUES":
+                        xml_bushings[child2.text] = child4.attrib
+                #if child3.text == "GE_VALUES":
+                    #print("a")
+            #xml_bushings.insert(0, child2.text)
+    print(xml_bushings)
+    
+    for row_idx in range(0, x1_sheet.nrows):    # Iterate through rows
+        for col_idx in range(0, x1_sheet.ncols):  # Iterate through columns
+            cell_obj = x1_sheet.cell(row_idx, col_idx).value  # Get cell object by row, col 
+            if cell_obj == "Bushing":
+                    for row_idx in range(row_idx+1, x1_sheet.nrows): # Iterate through columns
+                        for key in xml_bushings.items():
+                            wb.cell(row = row_idx, column = col_idx).value = key
+        
+    
+    y1_workbook.save('C:\\Users\\JCHAV106\\git\\Scripts\\test\\src\\test_data\\Extract_bushing_rates_tool - Copy3.xlsx')                       
+"""wb.cell(row = 1, column = 2).value = 10
+b = wb.cell(row = 1, column = 2).value
+wb['G12'] = 4
+a = wb['G12'].value
+print(b)"""
+    
+
+
+tree = ET.parse('C:\\Users\\JCHAV106\\git\\Scripts\\test\\src\\test_data\\INLINEC.xml')
 root = tree.getroot()
 
-x1_sheet = x1_workbook.sheet_by_index(0)
-
-#Iterate over inline.xml file to obtain all the bushing names 
-for child in root.iter("NVHC_PROPERTY"):
-    for child2 in child.iter("NAME"):
-        xml_bushings.insert(0, child2.text)
-print(xml_bushings)
-
-cont = 0
-bushings_dict = {}
-num_cols = x1_sheet.ncols  # Get  number of columns
-
-for row_idx in range(0, x1_sheet.nrows):    # Iterate through rows
-    for col_idx in range(0, num_cols):  # Iterate through columns
-        cell_obj = x1_sheet.cell(row_idx, col_idx).value  # Get cell object by row, col 
-        if cell_obj == "Bushing":
-            for col_idx in range(col_idx, num_cols):    # Iterate through rows
-                for row_idx in range(row_idx, x1_sheet.nrows): # Iterate through columns
-                    cell_obj2 = x1_sheet.cell(row_idx, col_idx).value
-                    for i in range(xml_bushings.__len__()):
-                        if cell_obj2 == xml_bushings[i]:
-                            r = row_idx 
-                            #print(cell_obj2, xml_bushings[i], i )
-                            for col_idx2 in range(col_idx+1, 10):    # Iterate through rows
-                                for row_idx in range(col_idx+1, 6):  # Iterate through columns
-                                    cell_obj3 = x1_sheet.cell(r, col_idx2).value
-                                    #print(cell_obj3,cell_obj2, xml_bushings[i], a + 1)
-                                    exl_bushings.insert(0, cell_obj3)
-                                    cont = cont + 1
-                                    if cont == 6:
-                                        cont = 0
-                                        bushings_dict[str(cell_obj2)] = exl_bushings
-                                        exl_bushings = [] 
-                                        break
-                                    
-                                    break
-
-print(bushings_dict)
-
-#Iterate trough all the NVHC PROPERTY tags contained in the file
-for child3 in root.iter("NVHC_PROPERTY"):
-    #Iterate trough all the NAME tags that are child of each NVHC PROPERTY tag
-    for child4 in child3.iter("NAME"):
-        #Iterate trough items of the bushing_dict to obtain their keys and values
-        for key,value in bushings_dict.items():
-            #Iterate to compare if the NAME obtained from the Bushing matches with one of the keys of the bushings_dict
-            if child4.text == key:
-                print(key)
-                #Accesign to each element of the list that is inside the found key
-                rt1 = bushings_dict[child4.text][0]
-                rt2 = bushings_dict[child4.text][1] 
-                rt3 = bushings_dict[child4.text][2] 
-                rt4 = bushings_dict[child4.text][3] 
-                rt5 = bushings_dict[child4.text][4] 
-                rt6 = bushings_dict[child4.text][5]
-                #Iterate trough all the PARAMETERS tags that are child of each NVHC PROPERTY tag  
-                for child4 in child3.iter("PARAMETERS"):
-                    #Iterates trough the content of the PARAMMETERS tag to find the busjing values(stiffnes(k) and ge) and replace each 
-                    #one of them with the corresponding value obtained from the excel file
-                    for child5 in child4:
-                        #Compare if the child tag of PARAMETERS matches with one of the values that gonna be replaced
-                        if child5.tag == "B_VALUES":
-                            child5.set("b1", str(0))
-                            child5.set("b2", str(0))
-                            child5.set("b3", str(0))
-                            child5.set("b4", str(0))
-                            child5.set("b5", str(0))
-                            child5.set("b6", str(0))
-                        #Write the new data to the same xml file or create a new with diferent name 
-                            tree.write(oname)  
-                            print(child5.attrib)
-                            
-                        elif child5.tag == "K_VALUES":
-                            child5.set("k1", str(rt6))
-                            child5.set("k2", str(rt5))
-                            child5.set("k3", str(rt4))
-                            child5.set("k4", str(rt3))
-                            child5.set("k5", str(rt2))
-                            child5.set("k6", str(rt1))
-                        #Write the new data to the same xml file or create a new with diferent name  
-                            tree.write(oname)  
-                            print(child5.attrib)
+xml2exl()
+#exl2xml()
