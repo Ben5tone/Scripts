508b16d9ab3056e6c78d9f2bbd0a95dcdeb8744e
JCHAV106 <JCHAV106@WGC100G8XN662.santafe.ford.com>
2018-02-20 16:30:16 -0600

    Added BImexMain

---
+++ test/src/test/BImexMain.py
@@ -0,0 +1,19 @@
+'''
+Created on 20/02/2018
+
+@author: JCHAV106
+'''
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
+root.title("Prueba")
+label = ttk.Label(root, text="BIm3x")
+label.pack()
+buton = ttk.Button(root, text="Ok")
+buton.pack()
+root.mainloop()
+
+if __name__ == '__main__':
+    pass
+
