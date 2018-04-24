2eec116434070173dc715a68fc6defebea707633
JCHAV106 <JCHAV106@WGC100G8XN662.santafe.ford.com>
2018-04-24 16:58:16 -0500

    Only the graphics are missing but everything is done

--- test/src/test/BushingsData.py
+++ test/src/test/BushingsData.py
@@ -8,6 +8,13 @@ from tkinter import messagebox, Toplevel
 from openpyxl import load_workbook
 import xml.etree.ElementTree as ET
 import os
+import ctypes.wintypes
+
+CSIDL_PERSONAL = 5       # My Documents
+SHGFP_TYPE_CURRENT = 0   # Get current, not default value
+
+buf= ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
+ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_PERSONAL, None, SHGFP_TYPE_CURRENT, buf)
 
 class Extract(object):
     
@@ -29,7 +36,7 @@ class Extract(object):
         print('Sheet Names',sheet_names)
         
         #Call the first sheet of the book
-        x1_sheet = x1_workbook.sheet_by_index(5)
+        x1_sheet = x1_workbook.sheet_by_index(0)
     
         #List variables to put the names of the bushing names contained in the xml and excel file
         xml_bushings = []
@@ -55,10 +62,10 @@ class Extract(object):
                                         r = row_idx 
                                         #print(cell_obj2, xml_bushings[i], i )
                                         for col_idx2 in range(col_idx+1, 10):    # Iterate through rows
-                                            for row_idx in range(col_idx+1, 6):  # Iterate through columns
+                                            for row_idx in range(col_idx+1, 18):  # Iterate through columns
                                                 cell_obj3 = x1_sheet.cell(r, col_idx2).value
                                                 #print(cell_obj3,cell_obj2, xml_bushings[i], a + 1)
-                                                exl_bushings.insert(0, cell_obj3)
+                                                exl_bushings.insert(exl_bushings.__len__(), cell_obj3)
                                                 cont = cont + 1
                                                 if cont == 6:
                                                     cont = 0
@@ -78,6 +85,7 @@ class Extract(object):
                 for key, value in bushings_dict.items():
                     #Iterate to compare if the NAME obtained from the Bushing matches with one of the keys of the bushings_dict
                     if child4.text == key:
+                        print('?????????????????????????')
                         print(key)
                         #Accesign to each element of the list that is inside the found key
                         rt1 = bushings_dict[child4.text][0]
@@ -86,30 +94,53 @@ class Extract(object):
                         rt4 = bushings_dict[child4.text][3] 
                         rt5 = bushings_dict[child4.text][4] 
                         rt6 = bushings_dict[child4.text][5]
+                        rt7 = bushings_dict[child4.text][6]
+                        rt8 = bushings_dict[child4.text][7] 
+                        rt9 = bushings_dict[child4.text][8] 
+                        rt10 = bushings_dict[child4.text][9] 
+                        rt11 = bushings_dict[child4.text][10] 
+                        rt12 = bushings_dict[child4.text][11]
+                        rt13 = bushings_dict[child4.text][12]
+                        rt14 = bushings_dict[child4.text][13] 
+                        rt15 = bushings_dict[child4.text][14] 
+                        rt16 = bushings_dict[child4.text][15] 
+                        rt17 = bushings_dict[child4.text][16] 
+                        rt18 = bushings_dict[child4.text][17]
                         #Iterate trough all the PARAMETERS tags that are child of each NVHC PROPERTY tag  
                         for child4 in child3.iter("PARAMETERS"):
                             #Iterates trough the content of the PARAMMETERS tag to find the busjing values(stiffnes(k) and ge) and replace each 
                             #one of them with the corresponding value obtained from the excel file
                             for child5 in child4:
                                 #Compare if the child tag of PARAMETERS matches with one of the values that gonna be replaced
-                                if child5.tag == "B_VALUES":
-                                    child5.set("b1", str(0))
-                                    child5.set("b2", str(0))
-                                    child5.set("b3", str(0))
-                                    child5.set("b4", str(0))
-                                    child5.set("b5", str(0))
-                                    child5.set("b6", str(0))
+                                if child5.tag == "K_VALUES":
+                                    child5.set("b1", str(rt1))
+                                    child5.set("b2", str(rt2))
+                                    child5.set("b3", str(rt3))
+                                    child5.set("b4", str(rt4))
+                                    child5.set("b5", str(rt5))
+                                    child5.set("b6", str(rt6))
                                     #Write the new data to the same xml file or create a new with diferent name 
                                     tree.write(xmlfo)  
                                     #print(child5.attrib)
                             
-                                elif child5.tag == "K_VALUES":
-                                    child5.set("k1", str(rt6))
-                                    child5.set("k2", str(rt5))
-                                    child5.set("k3", str(rt4))
-                                    child5.set("k4", str(rt3))
-                                    child5.set("k5", str(rt2))
-                                    child5.set("k6", str(rt1))
+                                elif child5.tag == "B_VALUES":
+                                    child5.set("ge1", str(rt7))
+                                    child5.set("ge2", str(rt8))
+                                    child5.set("ge3", str(rt9))
+                                    child5.set("ge4", str(rt10))
+                                    child5.set("ge5", str(rt11))
+                                    child5.set("ge6", str(rt12))
+                                    #Write the new data to the same xml file or create a new with diferent name  
+                                    tree.write(xmlfo)  
+                                    #print(child5.attrib)
+                                
+                                elif child5.tag == "GE_VALUES":
+                                    child5.set("ge1", str(rt13))
+                                    child5.set("ge2", str(rt14))
+                                    child5.set("ge3", str(rt15))
+                                    child5.set("ge4", str(rt16))
+                                    child5.set("ge5", str(rt17))
+                                    child5.set("ge6", str(rt18))
                                     #Write the new data to the same xml file or create a new with diferent name  
                                     tree.write(xmlfo)  
                                     #print(child5.attrib)
@@ -136,7 +167,7 @@ class Extract(object):
        
         #Open the workbook to read in
         y1_workbook = load_workbook(template)
-        wb = y1_workbook['CM6']
+        wb = y1_workbook['CM1']
     
         #List variables to put the names of the bushing names contained in the xml and excel file
         xml_bushings = {}
@@ -238,13 +269,13 @@ class Extract(object):
         if exlfo != None:
             y1_workbook.save(exlfo)
         else:
-            save_path = 'Extract_bushing_rates_tool' + '(' + str(save_num) + ')' + '.xlsx'
-            exists = os.path.exists(save_path)
+            #save_path = 'Extract_bushing_rates_tool' + '(' + str(save_num) + ')' + '.xlsx'
+            exists = os.path.exists(buf.value + '/Extract_bushings_rates_tool' + '(' + str(save_num) + ')' + '.xlsx')
             while exists == True:
                 save_num += 1
-                save_path = 'Extract_bushing_rates_tool' + '(' + str(save_num) + ')' + '.xlsx'
-                exists = os.path.exists(save_path)
-            save = os.path.join(dirname,save_path)
+                #save_path = 'Extract_bushing_rates_tool' + '(' + str(save_num) + ')' + '.xlsx'
+                exists = os.path.exists(buf.value + '/Extract_bushings_rates_tool' + '(' + str(save_num) + ')' + '.xlsx')
+            save = os.path.join(dirname,buf.value + '/Extract_bushings_rates_tool' + '(' + str(save_num) + ')' + '.xlsx')
             y1_workbook.save(save)
 
         if exlfo != None:
@@ -253,7 +284,7 @@ class Extract(object):
 
             x1_workbook = xlrd.open_workbook(save)
 
-        x1_sheet = x1_workbook.sheet_by_index(5)
+        x1_sheet = x1_workbook.sheet_by_index(0)
 
         for control_group2 in root:
             if control_group2.tag == "NVHC_CONTROL_GROUP":
@@ -276,50 +307,50 @@ class Extract(object):
                 if x1_sheet.cell(row_idx,3).value == b_k:
                     for col_idx in range(5,x1_sheet.ncols+1):
                         if col_idx == 5:
-                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['B_VALUES']['b1']))
-                        if col_idx == 6:
-                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['B_VALUES']['b2']))
-                        if col_idx == 7:
-                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['B_VALUES']['b3']))
-                        if col_idx == 8:
-                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['B_VALUES']['b4']))
-                        if col_idx == 9:
-                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['B_VALUES']['b5']))
-                        if col_idx == 10:
-                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['B_VALUES']['b6']))
-                        if col_idx == 11:
-                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['GE_VALUES']['ge1']))
-                        if col_idx == 12:
-                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['GE_VALUES']['ge2']))
-                        if col_idx == 13:
-                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['GE_VALUES']['ge3']))
-                        if col_idx == 14:
-                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['GE_VALUES']['ge4']))
-                        if col_idx == 15:
-                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['GE_VALUES']['ge5']))
-                        if col_idx == 16:
-                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['GE_VALUES']['ge6']))
-                        if col_idx == 17:
                             wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['K_VALUES']['k1']))
-                        if col_idx == 18:
+                        if col_idx == 6:
                             wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['K_VALUES']['k2']))
-                        if col_idx == 19:
+                        if col_idx == 7:
                             wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['K_VALUES']['k3']))
-                        if col_idx == 20:
+                        if col_idx == 8:
                             wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['K_VALUES']['k4']))
-                        if col_idx == 21:
+                        if col_idx == 9:
                             wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['K_VALUES']['k5']))
-                        if col_idx == 22:
+                        if col_idx == 10:
                             wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['K_VALUES']['k6']))
+                        if col_idx == 11:
+                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['B_VALUES']['b1']))
+                        if col_idx == 12:
+                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['B_VALUES']['b2']))
+                        if col_idx == 13:
+                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['B_VALUES']['b3']))
+                        if col_idx == 14:
+                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['B_VALUES']['b4']))
+                        if col_idx == 15:
+                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['B_VALUES']['b5']))
+                        if col_idx == 16:
+                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['B_VALUES']['b6']))
+                        if col_idx == 17:
+                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['GE_VALUES']['ge1']))
+                        if col_idx == 18:
+                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['GE_VALUES']['ge2']))
+                        if col_idx == 19:
+                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['GE_VALUES']['ge3']))
+                        if col_idx == 20:
+                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['GE_VALUES']['ge4']))
+                        if col_idx == 21:
+                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['GE_VALUES']['ge5']))
+                        if col_idx == 22:
+                            wb.cell(row = row_idx+1, column = col_idx, value = float(bushingsss[b_k]['GE_VALUES']['ge6']))
         if exlfo != None:
             y1_workbook.save(exlfo)
         else:
             
-            save_path = 'Extract_bushing_rates_tool' + '(' + str(save_num) + ')' + '.xlsx'
-            exists = os.path.exists(save_path)
+            #save_path = 'Extract_bushing_rates_tool' + '(' + str(save_num) + ')' + '.xlsx'
+            exists = os.path.exists(buf.value + '/Extract_bushings_rates_tool' + '(' + str(save_num) + ')' + '.xlsx')
             while exists == True:
                 save_num += 1
-                save_path = 'Extract_bushing_rates_tool' + '(' + str(save_num) + ')' + '.xlsx'
-                exists = os.path.exists(save_path)
-            save = os.path.join(dirname,save_path)
+                #save_path = 'Extract_bushing_rates_tool' + '(' + str(save_num) + ')' + '.xlsx'
+                exists = os.path.exists(buf.value + '/Extract_bushings_rates_tool' + '(' + str(save_num) + ')' + '.xlsx')
+            save = os.path.join(dirname,buf.value + '/Extract_bushings_rates_tool' + '(' + str(save_num) + ')' + '.xlsx')
             y1_workbook.save(save)
